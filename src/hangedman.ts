3d315b41d49571b5c2cde403a66cca9a21c2e5fb
Domenico Posillipo <domposi@gmail.com>
2024-12-10 18:23:26 +0100

    Added the documentation

--- src/hangedman.ts
+++ src/hangedman.ts
@@ -1,6 +1,9 @@
+// Import the readlineSync module to handle user input
 import readlineSync from 'readline-sync';
+// Import the list of words from an external dictionary file
 import { wordsList } from './dictionary_small';
-//array of all the letter in the alphabet
+
+// Array containing all letters of the alphabet
 const alphabet: string[] = [
     'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l',
@@ -9,30 +12,38 @@ const alphabet: string[] = [
     'y', 'z'
 ];
 
-//length of the word to find
-let wordLen: number = Number(readlineSync.question('Numero lettere: '));
-//letters present in the words
+// Initial Variables
+// Length of the word to guess
+let wordLen: number = Number(readlineSync.question('Number of letters: '));
+// Template for the word to find, initially empty
 let wordToFindTemplate: string = '';
+// Array representing the word to find, based on the template (with unguessed letters as '_')
 let wtfArr: string[] = [...wordToFindTemplate];
-//letters not present in the word 
+// Array of letters that are not present in the word
 let outArr: string[] = [];
-//number of the letter in wordToFindTemplate 
+// Number of correctly guessed letters (excluding '_')
 let numOfLetter: number = 0;
-//populating numOfLetter
+
+// Populate numOfLetter by counting the letters already guessed in the template
 wtfArr.forEach((letter) => {
     if (letter !== '_') numOfLetter++;
-})
-//list of the words with the correct length
+});
+
+// List of words with the correct length
 let currentWordsList: string[] = [];
-//list of the words that match the word to find
+// List of words matching the template of the word to find
 let currentWordToFindList: string[] = [];
-//list of all the occurrence per letter
+// Array to store the occurrence count of each letter
 const occurrenceCount: number[] = [];
-//list in decremental order of the most present letter
+// Array of the letters sorted by their frequency in descending order
 const mostPresentLetter: string[] = [];
-//populating the current word list
-wordsList.forEach((word) => { if (word.length === wordLen) currentWordsList.push(word) })
-//populating occurrence count
+
+// Populate currentWordsList with words that have the correct length
+wordsList.forEach((word) => { 
+    if (word.length === wordLen) currentWordsList.push(word);
+});
+
+// Populate occurrenceCount with how many times each letter appears in the words of currentWordsList
 for (let letter of alphabet) {
     let count = 0;
     for (let word of currentWordsList) {
@@ -40,34 +51,64 @@ for (let letter of alphabet) {
     }
     occurrenceCount.push(count);
 }
-//populating most present letter
+
+// Populate mostPresentLetter by sorting the letters based on their frequency, from most to least frequent
 for (let i = 0; i < occurrenceCount.length; i++) {
     mostPresentLetter.push(alphabet[occurrenceCount.indexOf(Math.max(...occurrenceCount))]);
     occurrenceCount[occurrenceCount.indexOf(Math.max(...occurrenceCount))] = 0;
 }
+
+/**
+ * Function that filters words based on the template of the word to find
+ * and the letters that are not present in the word.
+ * 
+ * @param {string[]} wtfArr - Array representing the word to find, with guessed letters and '_'.
+ * @param {string[]} outArr - Array containing the letters not present in the word.
+ * @param {number} numOfLetter - The number of correctly guessed letters in the template.
+ */
 const WordsList: (wtfArr: string[], outArr: string[], numOfLetter: number) => void = (wtfArr, outArr, numOfLetter) => {
     currentWordsList.forEach((word) => {
         let skip: boolean = false;
+        
+        // If there are letters that should not be in the word, skip words containing those letters
         if (outArr.length > 0) {
             outArr.forEach((letter) => {
                 if (word.includes(letter)) skip = true;
-            })
+            });
         }
         if (skip) return;
+
+        // Count how many letters in the word match the template of the word to find
         let count: number = 0;
         for (let index in wtfArr) {
             if (wtfArr[index] !== '_' && word.charAt(Number(index)) === wtfArr[index]) count++;
         }
+
+        // If the word matches the number of correctly guessed letters, add it to the list of possible words
         if (count === numOfLetter) currentWordToFindList.push(word);
-    })
+    });
+
+    // Display the list of most frequent letters
     console.log(mostPresentLetter);
+    // Display the list of words that could match the template
     console.log(currentWordToFindList);
+
+    // Reset the list of found words for the next iteration
     currentWordToFindList = [];
 }
+
+/**
+ * Function to set up a new round of the game.
+ * Resets variables and generates a new list of words based on the selected word length.
+ */
 const setNew: () => void = () => {
-    wordLen = Number(readlineSync.question('Numero lettere: '));
+    wordLen = Number(readlineSync.question('Number of letters: '));
     currentWordsList = [];
-    wordsList.forEach((word) => { if (word.length === wordLen) currentWordsList.push(word) })
+    
+    // Populate currentWordsList with words that have the correct length
+    wordsList.forEach((word) => { if (word.length === wordLen) currentWordsList.push(word) });
+
+    // Create the template for the word to find (composed of '_')
     for (let i = 0; i < wordLen; i++) {
         wordToFindTemplate += '_';
     }
@@ -77,35 +118,53 @@ const setNew: () => void = () => {
     console.log(currentWordToFindList);
     console.log('-----------------');
 }
+
+// Initial option to start the game
 let option: string = 'new';
+
+// Main game loop
 while (option !== 'stop') {
     switch (option) {
         case 'new':
+            // Start a new round: reset the state and begin a new game
             setNew();
             WordsList(wtfArr, outArr, numOfLetter);
-            option = 'cont'
+            option = 'cont'; // Continue the game
             break;
+        
         case 'cont':
             numOfLetter = 0;
-            let template: string | string[] = readlineSync.question('Template parola: ');
+            let template: string | string[] = readlineSync.question('Word template: ');
+            
             if(template === 'new'){
+                // If the user wants to start a new round
                 option = 'new';
                 break;
             }
+            
+            // Convert the template string into an array to update the word template
             template = [...template];
+            
+            // Update the wtfArr template with the newly guessed letters
             for (let i = 0; i < template.length; i += 2) {
                 wtfArr[Number(template[i]) - 1] = template[i + 1];
             }
-            //populating numOfLetter
+
+            // Populate numOfLetter with the number of correctly guessed letters in the template
             wtfArr.forEach((letter) => {
                 if (letter !== '_') numOfLetter++;
-            })
+            });
+
+            // If the user hasn't guessed a letter, ask for a letter not in the word
             if(template.length === 0){
-                let tempOut: string = readlineSync.question('Lettera non presente nella parola: ');
+                let tempOut: string = readlineSync.question('Letter not present in the word: ');
                 if(tempOut !== '') outArr.push(tempOut);
             }
-            WordsList(wtfArr, outArr,numOfLetter);
+
+            // Filter the word list based on the updated template
+            WordsList(wtfArr, outArr, numOfLetter);
             break;
+
         default:
             break;
     }
